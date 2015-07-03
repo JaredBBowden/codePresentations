519feebe7abaa2aee581da06536f8c5af764ea45
Jared Bowden <jaredbbowden@gmail.com>
2015-07-03 11:11:26 -0500

    This is a commit that has come from Atom

--- seed.py
+++ seed.py
@@ -1,8 +1,18 @@
+#!/anaconda/bin/ipython
+"""
+Adding some code to play with the Atom editor
+"""
+
 import pandas as pd
 import matplotlib.pylab as plt
 
 # Make a test dataframe
 df = pd.DataFrame({"first":range(50),"second":range(50)})
+
+print "Plotting results"
+
+for x in range(200):
+    print x
 
 # Plot the results
 plt.scatter(df["first"], df["second"])
