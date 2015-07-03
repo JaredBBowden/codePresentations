cd8e101d403d4d63a6601edcb833dad678b48b0f
Jared Bowden <jaredbbowden@gmail.com>
2015-07-03 11:20:21 -0500

    Adding title labels

--- seed.py
+++ seed.py
@@ -1,7 +1,7 @@
 #!/anaconda/bin/ipython
 """
 Adding some code to play with the Atom editor.
-Also testing the abilities of pycharm
+Also testing the abilities of pycharm.
 """
 
 import pandas as pd
@@ -10,10 +10,13 @@ import matplotlib.pylab as plt
 # Make a test dataframe
 df = pd.DataFrame({"first":range(50),"second":range(50)})
 
+# Plot the results
 print "Plotting results"
 
-# Plot the results
 plt.scatter(df["first"], df["second"])
 plt.title("First and second")
+plt.xlabel("First")
+plt.ylabel("Second")
+
 plt.show()
 plt.close()
