9887e670716a6e6541ba3b40d29fab2dcaf53ff7
Jared Bowden <jaredbbowden@gmail.com>
2015-07-11 15:35:51 -0500

    Cleans out some junk

--- iris_ML.py
+++ iris_ML.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-import datetime
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
 
@@ -13,11 +12,13 @@ attributes = iris.data
 target = iris.target
 
 # Spilt into test and train groups
-X_train, X_test, y_train, y_test = train_test_split(attributes, target, train_size=0.8, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(attributes, target,
+train_size=0.8, random_state = 42)
 
 # Make a rf object
 # Not really clear
-clf = clf = RandomForestClassifier(n_estimators = 10, criterion="gini", max_depth=None, n_jobs = -1, random_state=42, verbose=1)
+clf = clf = RandomForestClassifier(n_estimators = 10, criterion="gini",
+max_depth=None, n_jobs = -1, random_state=42, verbose=1)
 
 # Train
 clf.fit(X_train, y_train)
@@ -28,8 +29,3 @@ y_pred = clf.predict(X_test)
 # Score
 print "/nScore:"
 print classification_report(y_test, y_pred, labels=None, target_names=None)
-
-this = pd.DataFrame(range(20))
-that = pd.DataFrame((range(20) * 2))
-
-print pd.merge(this, that)
